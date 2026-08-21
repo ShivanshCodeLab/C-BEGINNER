d429e61ea91e77c7a3dc9c9c31fe6b397affb731
shivansh-Dseu <shivanshkumar.8595@gmail.com>
2026-08-21 19:27:15 +0530

    21Aug2026

--- 1_Chapters/chapter 3/08_quiz.c
+++ 1_Chapters/chapter 3/08_quiz.c
@@ -18,24 +18,24 @@ int main()
     printf("Enter Your Marks: ");
     scanf("%d", &marks);
 
-    if(marks<=100 && marks>=90){
+    if(marks<=100 && marks>=90)
         grade = 'A';
-    }
-    else if(marks<=90 && marks>=80){
+    
+    else if(marks<=90 && marks>=80)
         grade = 'B';    
-    }
-    else if(marks<=80 && marks>=70){
+    
+    else if(marks<=80 && marks>=70)
         grade = 'C';
-    }
-    else if(marks<=70 && marks>=60){
+    
+    else if(marks<=70 && marks>=60)
         grade = 'D';
-    }
-    else if(marks<=60 && marks>=50){
+    
+    else if(marks<=60 && marks>=50)
         grade = 'E';
-    }
-    else{
+    
+    else
         grade = 'F';
-    }
+    
     printf("Your Grade : %c", grade);
 
     return 0;
